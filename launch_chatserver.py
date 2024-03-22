011bb8d800e4f77feb52014aa12610b495216977
mlinegar <mitchelllinegar@gmail.com>
2024-03-22 10:15:01 -0700

    upload large

--- launch_chatserver.py
+++ launch_chatserver.py
@@ -1,3 +1,4 @@
 from lmql.lib.chat import chatserver
 
-chatserver('chat.lmql', port=12345, host='0.0.0.0').run()
+chatserver('chat.lmql', port=18089, host='0.0.0.0').run()
+# chatserver('chat.lmql', port=18089, host='localhost').run()
