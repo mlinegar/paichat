ae5e2b82fb1dfabb9a36bdf88f808396cfd73fb6
mlinegar <mitchelllinegar@gmail.com>
2024-03-28 12:36:57 -0700

    V0.2. Major updates: speed eup, better logging.

--- launch_chatserver.py
+++ launch_chatserver.py
@@ -1,3 +1,23 @@
-# from lmql.lib.chat import chatserver
-from chat import chatserver
-chatserver('chat.lmql', port=18089, host='0.0.0.0').run()
+import lmql
+import sys
+import os
+file = "chat.lmql"
+# works
+from lmql.lib.chat import chatserver
+chatserver(file, port=18089, host='0.0.0.0').run()
+
+# from chat.output import ChatMessageOutputWriter, MessageDecorator
+
+# message = MessageDecorator()
+
+# from chat.chatserver import ChatServer
+
+# def chatserver(*args, **kwargs):
+#     """
+#     Constructs a chatserver instance with the given arguments.
+#     """
+#     return ChatServer(*args, **kwargs)
+
+# if __name__ == "__main__":
+#     server = chatserver(file, port=18089, host='0.0.0.0')
+#     server.run()
